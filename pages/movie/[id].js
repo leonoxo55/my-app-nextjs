84af08b4f834ed4e8df3f684297c8be1e7f3d8e7
Alexsander <alexsander.zuniga@readinessit.com>
2020-02-19 09:53:39 -0300

    routing system with movies

---
+++ pages/movie/[id].js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Menu from '../../components/Menu';
+import {useRouter} from 'next/router';
+
+export default function movie(){
+
+    const router = useRouter();
+    const {id} = router.query;
+
+    return (
+        <div>
+            <Menu />
+            <h2 style={{textAlign: 'center'}} >Estas viendo.. </h2>
+            <h1 style={{textAlign: 'center'}} > {id}</h1>
+        </div>
+    )
+}
--- pages/movies.js
+++ pages/movies.js
@@ -1,11 +1,31 @@
-import Menu from '../components/Menu'
+import Link from 'next/link';
+import Menu from '../components/Menu';
 
 export default function movies () {
 
+    const movies = [
+        {
+            id: 'joker',
+            name: 'Joker'
+        },
+        {
+            id: 'spider',
+            name: 'Spider-man'
+        },
+        {
+            id: 'batman',
+            name: 'Batman'
+        }
+    ]
     return (
         <div>
             <Menu />
             <h1 style={{textAlign:'center'}} >Estamos en la página de películas</h1>
+            {movies.map((movie, index) => (
+                <Link href='/movie/[movie]' as={`/movie/${movie.id}`} key={index} >
+                    <a style={{marginRight: 20}}>{movie.name}</a>
+                </Link>
+            ))}
         </div>
     )
 }
